a2f5fa64de5722e4bf5093bf1a44671cdedba142
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2024-01-15 17:44:33 -0500

    add helper function

--- hw2/src/gate.py
+++ hw2/src/gate.py
@@ -16,9 +16,22 @@ def get_stats(file):
   data = DATA(src = file)
   print(data.mid().cells)
   
+def help():
+    print("OPTIONS:")
+    print("  -h --help     show help                       = false")
+    print("  -f --file     csv data file name              = ../../data/auto93.csv")
+    print("  -t --todo     todo an action command          = todo")
+    print("  -c --cohen    small effect size               = .35")
+    print("  -k --k        low class frequency kludge      = 1")
+    print("  -m --m        low attribute frequency kludge  = 2")
+    print("  -s --seed     random number seed              = 31210")
+    sys.exit(0)
+  
 def arg_parser(argv):
   args = {}
   for i in range(1, len(argv), 2):
+    if argv[i]=='-h' or argv[i]=='--help':
+      help()
     if argv[i]=='-f' or argv[i]=='--file':
       args['file'] = argv[i+1]
     elif argv[i]=='-t' or argv[i]=='--todo':
