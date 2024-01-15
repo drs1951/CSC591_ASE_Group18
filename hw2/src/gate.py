1be394c6940f8018f7cfa1ef7955eb57e7e8c6ae
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2024-01-15 16:40:48 -0500

    add argument parser

--- hw2/src/gate.py
+++ hw2/src/gate.py
@@ -3,6 +3,9 @@ import random
 import argparse
 from num import NUM
 from sym import SYM
+from col import COLS
+from row import ROW
+from tests import run_tests
 
 # Assuming global settings are set somewhere in the code
 the = {
@@ -13,45 +16,6 @@ the = {
 }
 
 random.seed(the["seed"])
-
-class COLS:
-    def __init__(self, row_cells):
-        self.x = {}
-        self.y = {}
-        self.all = []
-        self.klass = None
-        self.names = row_cells
-
-        for at, txt in enumerate(row_cells):
-            # If the column name is uppercase, use NUM, otherwise use SYM
-            col = NUM(txt, at) if txt[0].isupper() else SYM(txt, at)
-            self.all.append(col)
-            # If column name does not end in 'X', add it to x or y
-            if not txt.endswith("X"):
-                if txt.endswith("!"):
-                    self.klass = col
-                # Add to y if it ends with '!', '+', or '-', otherwise add to x
-                if any(txt.endswith(special) for special in ["!", "+", "-"]):
-                    self.y[at] = col
-                else:
-                    self.x[at] = col
-
-    def add(self, row):
-        merged_x_y = {**self.x, **self.y}
-        # Assuming row is an instance of ROW class and has a 'cells' attribute which is a list or dict
-        for _,col in merged_x_y.items():
-            value = row.cells[col.txt]  # Get the value from the row
-            col.add(value)  # Use the 'add' method of NUM or SYM to add the value
-        return row
-class ROW:
-    def __init__(self, cells):
-        self.cells = cells
-
-# # Example usage:
-# # If 't' is a dictionary where keys are column names and values are the data for those columns:
-# row_data = {"Name": "Alice", "Age": 30, "Score": 85.5}
-# row = ROW(row_data)
-
 
 import csv
 import random
@@ -97,11 +61,22 @@ class DATA:
         for _,col in target_cols.items():
             u[col.txt] = col.mid()
         return ROW(u)
+      
+def get_stats(data):
+  print(data.mid().cells)
 
+def main():
+    parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--file', '-f', help = "file path", type = str)
-args = parser.parse_args()
-d = DATA(src = args.file)
-print(d.mid().cells)
+    parser.add_argument('--file', '-f', help = "file path", type = str)
+    parser.add_argument('--todo', '-t', help = "action that need to be performed", type = str)
+    args = parser.parse_args()
+    data = DATA(src = args.file)
+    if (args.todo=='stats'):
+      get_stats(data)
+    else:
+      run_tests(args.todo)
+    
+if __name__ == '__main__':
+    # This code won't run if this file is imported.
+    main()
