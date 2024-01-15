78044cc36969df1f378ab3e2ea7135f47f366d2f
“MitaliSethi” <msethi@ncsu.edu>
2024-01-15 18:10:17 -0500

    added sym test

---
+++ hw2/src/main.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("../CSC591_ASE_Group18/")
+from hw2.test.tests import *
+
+run_tests("all")
--- hw2/test/tests.py
+++ hw2/test/tests.py
@@ -1,11 +1,20 @@
-def t1():
-  print('test1')
+from hw2.src.num import *
+from hw2.src.sym import *
+
+def eg_sym():
+    s = SYM()
+    for x in ["a", "a", "a", "a", "b", "b", "c"]:
+        s.add(x)
+    mode = s.mid()
+    print(mode)
+    return mode == "a"
 
 def run_all_tests():
+  print(eg_sym())
   print('all tests')
 
 def run_tests(test_name):
   if (test_name=='all'):
     run_all_tests()
-  elif (test_name=='t1'):
-    t1()
+  elif (test_name=='sym'):
+    print(eg_sym())
