3b7136090c8ac316b0bc851a50bec2836e9c55a2
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2024-01-15 22:33:37 -0500

    Update tests.py

--- hw2/test/tests.py
+++ hw2/test/tests.py
@@ -1,5 +1,7 @@
 from hw2.src.num import *
 from hw2.src.sym import *
+import random
+from hw2.src.data import *
 
 def eg_sym():
     s = SYM()
@@ -9,8 +11,23 @@ def eg_sym():
     print(mode)
     return mode == "a"
 
+def eg_num():
+    e = NUM()
+    for _ in range(1000):
+        e.add(random.normalvariate(10, 2))
+    mu = e.mid()
+    print(mu)
+    return 9.9 < mu < 10.1
+
+def eg_csv():
+    n = 0
+    d = DATA('../../data/auto93.csv')
+
+    return len(d.rows)-1 == 398
+
 def run_all_tests():
   print(eg_sym())
+  print(eg_num())
   print('all tests')
 
 def run_tests(test_name):
@@ -18,3 +35,7 @@ def run_tests(test_name):
     run_all_tests()
   elif (test_name=='sym'):
     print(eg_sym())
+  elif (test_name=='num'):
+     print(eg_num())
+  elif (test_name=='csv'):
+     print(eg_csv())
