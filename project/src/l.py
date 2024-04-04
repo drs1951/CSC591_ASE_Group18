b4d29478aaef2efe8bb4463796a4533301c399f9
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2024-04-04 15:16:26 -0400

    init with rrp code

---
+++ project/src/l.py
@@ -0,0 +1,89 @@
+import random
+import math
+from hw8.config import *
+from row import *
+
+def any_item(lst):
+    return random.choice(lst)
+
+def o(t, ndecs=None):
+    if isinstance(t, float):
+        return rnd(t, ndecs)
+    if isinstance(t, ROW):
+        return str(t.cells) + " "
+    if not isinstance(t, dict) and not isinstance(t, list):
+        return str(t)
+    u = []
+    for k in t:
+        if not str(k).startswith("_"):
+            if isinstance(t, list):
+                u.append(o(t[k], ndecs))
+            else:
+                u.append("{}: {}".format(o(k, ndecs), o(t[k], ndecs)))
+    return "{" + ", ".join(u) + "}"
+
+def rnd(n, ndecs=2):
+    if not isinstance(n, float):
+        return n
+    if n.is_integer():
+        return int(n)
+    mult = 10 ** ndecs
+    return math.floor(n * mult + 0.5) / mult
+
+def score(t, goal, LIKE, HATE):
+    like = hate = 0
+    tiny = 1E-30
+    for klass, n in t.items():
+        if klass == goal:
+            like += n
+        else:
+            hate += n
+    like = like / (LIKE + tiny)
+    hate = hate / (HATE + tiny)
+    if hate > like:
+        return 0
+    else:
+        return like ** the.support / (like + hate)
+    
+def entropy(t):
+    n = sum(t.values())  # Total count of items
+    e = 0  # Entropy
+    for v in t.values():
+        p = v / n  # Probability of each item
+        e = e - p * math.log(p, 2)  # Accumulate entropy
+    return e, n
+
+def slice(t, start, stop=None, step=1):
+    if start and start < 0:
+        start = len(t) + start
+    if stop and stop < 0:
+        stop = len(t) + stop
+    return t[start:stop:step]
+
+
+def shuffle(t):
+    t=t[1:]
+    u = t[:]  
+    random.shuffle(u)
+    return u
+
+def score(t, goal, LIKE, HATE):
+    like = hate = 0
+    tiny = 1E-30
+    # print(t)
+    for klass, n in t.items():
+        if klass == goal:
+            like += n
+        else:
+            hate += n
+    like, hate = like / (LIKE + tiny), hate / (HATE + tiny)
+    if hate >= like:
+        return 0
+    else:
+        return like ** the.support / (like + hate)
+    
+def powerset(s):
+    t = [[]]
+    for item in s:
+        t += [subset + [item] for subset in t]
+    return t
---
+++ project/src/row.py
@@ -0,0 +1,29 @@
+from hw8.config import *
+
+class ROW:
+    def __init__(self, cells):
+        self.cells = cells
+
+    def d2h(self, data, p=2):
+        d, n = 0, 0
+        for col in data.cols.y.values():
+            x = self.cells[col.at]
+            if x is None:
+                print("?", end="")
+            else:
+                n += 1
+                d += abs(col.heaven - col.norm(self.cells[col.at])) ** p
+        return (d / n) ** (1 / p) if n > 0 else 0
+
+    def dist(self, other, data, p=None):
+        if p is None:
+            p = the.p 
+        d, n = 0, 0
+        for col in data.cols.x.values():
+            n += 1
+            d += col.dist(self.cells[col.at], other.cells[col.at]) ** p
+        return (d / n) ** (1 / p) if n > 0 else 0
+
+    def neighbors(self, data, rows=None):
+        rows = rows if rows is not None else data.rows[1:]
+        return sorted(rows, key=lambda row: self.dist(row, data))
